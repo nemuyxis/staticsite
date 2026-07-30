8ca9651ccc4b53597e106b27d7d9eefbe240d3b9
nemuyxis <nemu.yxis@gmail.com>
2026-07-30 00:29:21 -0400

    CH2 L6 TextNode to HTMLNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -16,6 +16,24 @@ class TestTextNode(unittest.TestCase):
     def test_url_none(self):
         node = TextNode("This is a text node", TextType.BOLD)
         self.assertIsNone(node.url)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_italic(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,13 @@
 from enum import Enum
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TextType(Enum):
-    TEXT = "text (plain)"
-    BOLD = "**Bold text**"
-    ITALIC = "_Italic text_"
-    CODE = "`Code text`"
-    LINK = "[anchor text](url)"
-    IMAGE = "![alt text](url)"
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode:
     def __init__(self, text: str, text_type: TextType, url: str | None = None) -> None:
@@ -25,3 +26,32 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if text_node.text_type not in TextType:
+        raise Exception("Not a valid TextType")
+
+    if text_node.text_type == TextType.TEXT:
+        leaf = LeafNode(None, text_node.text)
+        return leaf
+
+    if text_node.text_type == TextType.BOLD:
+        leaf = LeafNode("b", text_node.text)
+        return leaf
+
+    if text_node.text_type == TextType.ITALIC:
+        leaf = LeafNode("i", text_node.text)
+        return leaf
+
+    if text_node.text_type == TextType.CODE:
+        leaf = LeafNode("code", text_node.text)
+        return leaf
+
+    if text_node.text_type == TextType.IMAGE:
+        leaf = LeafNode("img", "", {"src": "image source url", "alt": "alt text of image",})
+        return leaf
+
+    if text_node.text_type == TextType.LINK:
+        leaf = LeafNode("a", text_node.text, {"href": text_node.url,})
+        return leaf
