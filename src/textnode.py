29fbd7ba0de062c0c2c094ce5f9a2f9a3b325aee
nemuyxis <nemu.yxis@gmail.com>
2026-07-31 01:19:10 -0400

    CH3 L1 Split delimiter

--- src/textnode.py
+++ src/textnode.py
@@ -55,3 +55,26 @@ def text_node_to_html_node(text_node: TextNode) -> LeafNode:
     if text_node.text_type == TextType.LINK:
         leaf = LeafNode("a", text_node.text, {"href": text_node.url,})
         return leaf
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        split_nodes = []
+        sections = node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+        
+
+        
