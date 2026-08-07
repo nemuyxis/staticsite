44796a17aa357ebf73d83be0134d9c0e71253c15
nemuyxis <nemu.yxis@gmail.com>
2026-08-07 00:46:28 -0400

    CH5 L1 Copy static

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
-def main():
-    print("hello world")
-    test = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(test)
+from copystatic import copy_files_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
+
+def main() -> None:
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
 
 main()
